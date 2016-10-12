57d3a764aa3adf9c1235d4f6aa35330242169108
fuhao <465476662@qq.com>
2016-10-12 16:57:31 +0800

    build

--- webpack/prod.js
+++ webpack/prod.js
@@ -7,5 +7,6 @@ module.exports = new WebpackConfig.Config().extend('./webpack/config-maker.js').
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
         }),
+        new webpack.HotModuleReplacementPlugin(),
     ]
 });
